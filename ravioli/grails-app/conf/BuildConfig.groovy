ea4b9efbcc7f47b910858d784151773c877da66d
noelwinstanley <noelwinstanley@daa9b71a-dffd-11de-b37d-f990b2f5f075>
2010-01-29 15:36:47 +0000

    tried removing xalan dep.

--- ravioli/grails-app/conf/BuildConfig.groovy
+++ ravioli/grails-app/conf/BuildConfig.groovy
@@ -3,7 +3,7 @@
 		inherits "global" // inherit Grails' default dependencies
 		log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 		repositories {
-			grailsHome()
+			//grailsHome()
 			// uncomment the below to enable remote dependency resolution
 			// from public Maven repositories
 			mavenCentral()
@@ -14,7 +14,8 @@
 		}
 		dependencies {
 			// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-			// runtime 'com.mysql:mysql-connector-java:5.1.5'
+			
+			// thought these were needed - seems not. guess they come with j1.6
 			//runtime 'xalan:serializer:2.7.1'
 			//runtime 'xalan:xalan:2.7.1'
 			//runtime 'net.sf.saxon:saxon:8.7'
@@ -22,7 +23,7 @@
 			//test 'ant:ant-junit:1.6.5'
 			
 			compile "joda-time:joda-time:1.6"
-			runtime 'com.mysql:mysql-connector-java:5.1.10'
+			runtime 'mysql:mysql-connector-java:5.1.10'
 		}
 	}
 	
